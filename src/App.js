94ce298ff664ae4a6ce72ece072366e676e27c6a
adamdawkins <adam@dragondrop.uk>
2020-09-08 22:05:45 +0100

    🎬 Initial selection / deselection of options

--- src/App.js
+++ src/App.js
@@ -1,24 +1,153 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import { VWGO20ME_5EDTA_6 } from "./data";
+import "./App.css";
+
+// UTILS
+
+//    without :: (x, [x]) -> [x]
+const without = (item, list) => list.filter(x => x !== item);
+
+//    contains :: (x, [x]) -> Boolean
+const contains = (item, list) => list.indexOf(item) > -1;
+
+//    unique :: [x] -> [x]
+const unique = list => [...new Set(list)];
+
+// DATA HANDLERS & SETUP
+
+//    init :: [OptionRow] => State
+const init = data => {
+  const state = {
+    options: {},
+    rules: {},
+    selectedOptions: [],
+    appliedRules: []
+  };
+
+  data.map(
+    ([
+      ruleCode,
+      ruleType,
+      optionCode,
+      isPrimary,
+      basicPrice,
+      vat,
+      defaultOption,
+      description
+    ]) => {
+      const option = state.options[optionCode] || { ruleIds: [] };
+      const newOption = Object.assign(option, {
+        id: optionCode,
+        price: basicPrice,
+        isDefault: defaultOption,
+        description: description,
+        ruleIds: option.ruleIds.concat([ruleCode])
+      });
+
+      state.options[optionCode] = newOption;
+
+      const rule = state.rules[ruleCode] || { optionIds: [] };
+      const newRule = Object.assign(rule, {
+        id: ruleCode,
+        type: ruleType,
+        optionIds: rule.optionIds.concat([optionCode])
+      });
+
+      state.rules[ruleCode] = newRule;
+
+      return true;
+    }
+  );
+
+  return state;
+};
+
+// STATE
+
+//    selectOption :: (id, state) -> State
+const selectOption = (id, state) => ({
+  ...state,
+  selectedOptions: state.selectedOptions.concat([id]),
+  appliedRules: unique(state.appliedRules.concat(state.options[id].ruleIds))
+});
+
+//    deselectOption :: (id, state) -> State
+const deselectOption = (id, state) => {
+  return {
+    ...state,
+    selectedOptions: without(id, state.selectedOptions),
+    appliedRules: getAppliedRules(state)
+  };
+};
+
+//    getAppliedRules :: State => [Int]
+const getAppliedRules = state =>
+  state.selectedOptions.length === 0
+    ? []
+    : unique(
+        state.selectedOptions.reduce(
+          (rules, id) => rules.concat(state.options[id].ruleIds),
+          []
+        )
+      );
+
+//    reducer :: (State, Action) -> State
+const reducer = (state, action) => {
+  console.log("REDUCER CALLED");
+  switch (action.type) {
+    case "TOGGLE_OPTION":
+      return contains(action.id, state.selectedOptions)
+        ? deselectOption(action.id, state)
+        : selectOption(action.id, state);
+  }
+};
+
+function Option({
+  id,
+  price,
+  isDefault,
+  description,
+  ruleIds,
+  isSelected,
+  onClick
+}) {
+  return (
+    <div onClick={onClick} className={`option ${isSelected ? "selected" : ""}`}>
+      {id}: {description}
+    </div>
+  );
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, VWGO20ME_5EDTA_6, init);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <div className="sidebar__options">
+          <h3>Selected Options</h3>
+          {state.selectedOptions.map(id => (
+            <Option key={id} {...state.options[id]} />
+          ))}
+        </div>
+        <div className="sidebar__rules">
+          <h3>Applied Rules</h3>
+          {state.appliedRules.map(id => (
+            <li key={id}>
+              {id}: {state.rules[id].type}
+            </li>
+          ))}
+        </div>
+      </div>
+      <div className="options">
+        {Object.keys(state.options).map(id => (
+          <Option
+            key={id}
+            {...state.options[id]}
+            isSelected={contains(id, state.selectedOptions)}
+            onClick={() => dispatch({ type: "TOGGLE_OPTION", id })}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,151 @@
+export const VWGO20ME_5EDTA_6 = [
+  [7223847, "OO", 961, false, 516.67, 103.33, false, "Metallic paint"],
+  [7223847, "OO", 1924, false, 516.67, 103.33, false, "Pearlescent paint"],
+  [7223847, "OO", 1977, false, 829.17, 165.83, false, "Premium paint"],
+  [7223847, "OO", 14752, false, 0, 0, true, "Solid paint"],
+  [
+    7223847,
+    "OO",
+    27336,
+    false,
+    516.67,
+    103.33,
+    false,
+    "Metallic - Reflex silver"
+  ],
+  [
+    7223847,
+    "OO",
+    54645,
+    false,
+    516.67,
+    103.33,
+    false,
+    "Metallic - Atlantic blue"
+  ],
+  [7223847, "OO", 64626, false, 308.33, 61.67, false, "Special solid paint"],
+  [7223847, "OO", 69465, false, 516.67, 103.33, false, "Pearl - Deep black"],
+  [
+    7223847,
+    "OO",
+    71081,
+    false,
+    516.67,
+    103.33,
+    false,
+    "Metallic - Indium grey"
+  ],
+  [7223847, "OO", 92805, false, 0, 0, false, "Solid - Urano grey"],
+  [
+    7223847,
+    "OO",
+    95049,
+    false,
+    516.67,
+    103.33,
+    false,
+    "Metallic - Tungsten silver"
+  ],
+  [
+    7223847,
+    "OO",
+    96002,
+    false,
+    308.33,
+    61.67,
+    false,
+    "Special solid - Pure white"
+  ],
+  [
+    7223847,
+    "OO",
+    97192,
+    false,
+    829.17,
+    165.83,
+    false,
+    "Premium paint - Oryx White"
+  ],
+  [
+    7223847,
+    "OO",
+    99520,
+    false,
+    308.33,
+    61.67,
+    false,
+    "Special solid - Tornado red"
+  ],
+  [
+    7223847,
+    "OO",
+    121755,
+    false,
+    516.67,
+    103.33,
+    false,
+    "Metallic - Cranberry red"
+  ],
+  [
+    7223848,
+    "OO",
+    95052,
+    false,
+    1791.67,
+    358.33,
+    false,
+    "Vienna leather upholstery + perforated heated front sports seats with manually adjustable lumbar support"
+  ],
+  [
+    7223848,
+    "OO",
+    95055,
+    false,
+    1791.67,
+    358.33,
+    false,
+    "Vienna leather - Black + heated front sports seats with lumbar support"
+  ],
+  [7223848, "OO", 99112, false, 0, 0, true, "Zoom/micro cloth upholstery"],
+  [7223848, "OO", 99121, false, 0, 0, false, "Zoom/micro cloth - Titan Black"],
+  [
+    7223848,
+    "OO",
+    99122,
+    false,
+    0,
+    0,
+    false,
+    "Zoom/micro cloth - Shetland beige"
+  ],
+  [
+    7223849,
+    "OO",
+    127918,
+    false,
+    512.5,
+    102.5,
+    false,
+    '17 "Karlskoga alloy wheel with anti theft wheel bolts'
+  ],
+  [
+    7223849,
+    "OO",
+    127919,
+    false,
+    512.5,
+    102.5,
+    false,
+    '17" Madrid alloy wheel with anti theft wheel bolts'
+  ],
+  [
+    7223849,
+    "OO",
+    134328,
+    false,
+    0,
+    0,
+    true,
+    '16" Woodstock alloy wheels with anti theft wheel bolts'
+  ]
+];
