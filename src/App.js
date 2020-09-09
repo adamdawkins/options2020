8279b2a4ff35542118be7ee16c0b01dd9588b1af
adamdawkins <adam@dragondrop.uk>
2020-09-09 13:32:31 +0100

    View a todo and see related options highlighted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { useReducer } from "react";
+import classNames from "classnames";
 import { VWGO20ME_5EDTA_6 } from "./data";
 import "./App.css";
 
 // UTILS
 
-//    without :: (x, [x]) -> [x]
-const without = (item, list) => list.filter(x => x !== item);
+// List Utils
 
 //    contains :: (x, [x]) -> Boolean
 const contains = (item, list) => list.indexOf(item) > -1;
 
 //    unique :: [x] -> [x]
 const unique = list => [...new Set(list)];
+
+//    without :: (x, [x]) -> [x]
+const without = (item, list) => list.filter(x => x !== item);
+
+// Object Utils
+
+//    collection :: {key: object} -> [object]
+const collection = obj => Object.keys(obj).map(key => obj[key]);
+
+//    safely return the property at a nested object path
+//    e.g. path(["a", "b", "c"], { a: { b: { c: "foo" } } }) => "foo"
+//    path :: ([String], object]) -> Any?
+const path = (props, object) => {
+  let currentProp = object;
+  let index = 0;
+
+  for (; index < props.length; ) {
+    if (currentProp == null) {
+      return;
+    }
+
+    currentProp = currentProp[props[index]];
+    index += 1;
+  }
+
+  return currentProp;
+};
 
 // DATA HANDLERS & SETUP
 
@@ -21,7 +48,8 @@ const init = data => {
     options: {},
     rules: {},
     selectedOptions: [],
-    appliedRules: []
+    appliedRules: [],
+    debug: {}
   };
 
   data.map(
@@ -62,30 +90,66 @@ const init = data => {
   return state;
 };
 
-// STATE
+// DEBUGGING HELPERS
+const debug = {
+  isRelated: (id, state) =>
+    state.debug.view &&
+    contains(id, relatedOptionIds(state.debug.view.id, state)),
+  isViewed: (id, state) => path(["debug", "view", "id"], state) === id
+};
 
-//    selectOption :: (id, state) -> State
-const selectOption = (id, state) => ({
-  ...state,
-  selectedOptions: state.selectedOptions.concat([id]),
-  appliedRules: unique(state.appliedRules.concat(state.options[id].ruleIds))
+// GETTERS
+//    rulesForOption :: (id, state) -> [Rule]
+const rulesForOption = (id, state) =>
+  state.options[id].ruleIds.map(id => state.rules[id]);
+
+//    optionsForRule :: (id, state) -> [Option]
+const optionsForRule = (id, state) =>
+  state.rules[id].optionIds.map(id => state.options[id]);
+
+// fills out the option with rules: [Rule]
+//    buildOption :: (id, state) -> Option
+const buildOption = (id, state) => ({
+  ...state.options[id],
+  rules: rulesForOption(id, state)
 });
 
-//    deselectOption :: (id, state) -> State
-const deselectOption = (id, state) => {
+//    relatedOptionIds :: (id, state) -> [Int]
+const relatedOptionIds = (id, state) =>
+  unique(
+    rulesForOption(id, state)
+      .map(({ optionIds }) => optionIds)
+      .flat()
+  );
+
+// ACTIONS
+
+//    selectOption :: (id, state) -> State
+const selectOption = (id, state) => {
+  const selectedOptions = state.selectedOptions.concat([id]);
   return {
     ...state,
-    selectedOptions: without(id, state.selectedOptions),
-    appliedRules: getAppliedRules(state)
+    selectedOptions,
+    appliedRules: getAppliedRules(selectedOptions, state)
   };
 };
 
-//    getAppliedRules :: State => [Int]
-const getAppliedRules = state =>
-  state.selectedOptions.length === 0
+//    deselectOption :: (id, state) -> State
+const deselectOption = (id, state) => {
+  const selectedOptions = without(id, state.selectedOptions);
+  return {
+    ...state,
+    selectedOptions,
+    appliedRules: getAppliedRules(selectedOptions, state)
+  };
+};
+
+//    getAppliedRules :: ([Int], State) => [Int]
+const getAppliedRules = (selectedOptions, state) =>
+  selectedOptions.length === 0
     ? []
     : unique(
-        state.selectedOptions.reduce(
+        selectedOptions.reduce(
           (rules, id) => rules.concat(state.options[id].ruleIds),
           []
         )
@@ -93,12 +157,16 @@ const getAppliedRules = state =>
 
 //    reducer :: (State, Action) -> State
 const reducer = (state, action) => {
-  console.log("REDUCER CALLED");
   switch (action.type) {
     case "TOGGLE_OPTION":
       return contains(action.id, state.selectedOptions)
         ? deselectOption(action.id, state)
         : selectOption(action.id, state);
+    case "DEBUG.VIEW_OPTION":
+      return {
+        ...state,
+        debug: { ...state.debug, view: { type: "option", id: action.id } }
+      };
   }
 };
 
@@ -109,44 +177,69 @@ function Option({
   description,
   ruleIds,
   isSelected,
-  onClick
+  onClick,
+  debug
 }) {
   return (
-    <div onClick={onClick} className={`option ${isSelected ? "selected" : ""}`}>
+    <div
+      onClick={onClick}
+      className={classNames("option", {
+        selected: isSelected,
+        viewed: debug.isViewed,
+        related: debug.isRelated
+      })}
+    >
       {id}: {description}
     </div>
   );
 }
 
-function App() {
-  const [state, dispatch] = useReducer(reducer, VWGO20ME_5EDTA_6, init);
+export const DebugView = ({ option }) => {
   return (
-    <div className="App">
-      <div className="sidebar">
-        <div className="sidebar__options">
-          <h3>Selected Options</h3>
-          {state.selectedOptions.map(id => (
-            <Option key={id} {...state.options[id]} />
-          ))}
-        </div>
-        <div className="sidebar__rules">
-          <h3>Applied Rules</h3>
-          {state.appliedRules.map(id => (
-            <li key={id}>
-              {id}: {state.rules[id].type}
+    <div>
+      <h2>
+        {option.id}: {option.description}
+      </h2>
+      <div>
+        <h3>Rules</h3>
+        <ul>
+          {option.rules.map(rule => (
+            <li key={rule.id}>
+              {rule.id} ({rule.type})
             </li>
           ))}
-        </div>
+        </ul>
       </div>
+
+      <h3>Related Options</h3>
+    </div>
+  );
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, VWGO20ME_5EDTA_6, init);
+  console.log(state);
+  return (
+    <div className="App">
       <div className="options">
-        {Object.keys(state.options).map(id => (
+        {collection(state.options).map(option => (
           <Option
-            key={id}
-            {...state.options[id]}
-            isSelected={contains(id, state.selectedOptions)}
-            onClick={() => dispatch({ type: "TOGGLE_OPTION", id })}
+            key={option.id}
+            onClick={() =>
+              dispatch({ type: "DEBUG.VIEW_OPTION", id: option.id })
+            }
+            {...option}
+            debug={{
+              isRelated: debug.isRelated(option.id, state),
+              isViewed: debug.isViewed(option.id, state)
+            }}
           />
         ))}
+      </div>
+      <div className="sidebar">
+        {state.debug.view && (
+          <DebugView option={buildOption(state.debug.view.id, state)} />
+        )}
       </div>
     </div>
   );
